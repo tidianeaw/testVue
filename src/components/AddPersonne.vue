874b63a7044fd9c7183d23ee2dcc8b11dea3c50e
tidianeaw <tidiane_aw@yahoo.fr>
2022-11-18 17:41:34 +0100

    6eme commit

--- src/components/AddPersonne.vue
+++ src/components/AddPersonne.vue
@@ -1,30 +1,82 @@
 <template>
   <div class="submit-form">
     <div v-if="!submitted">
-      
-        <div class="col-sm-9">
-          <label for="name" class="form-label">Surname</label>
-          <input type="text" class="form-control" id="surname" name="surname" v-model="personne.surname">
-        </div>
 
-        <div class="col-sm-9">
-          <label for="surname" class="form-label">Name</label>
-          <input type="text" class="form-control" id="name" name="name" v-model="personne.name">
-        </div>
-
-        <div class="col-sm-9">
-          <label for="phone" class="form-label">Phone</label>
-          <input type="text" class="form-control" id="phone" name="phone" v-model="personne.phone">
-        </div>
+        <input type="hidden" name="id" v-model="personne.id">
         
-        <div class="col-sm-9">
-          <label for="city" class="form-label">City</label>
-          <input type="text" class="form-control" id="city" name="city" v-model="personne.city">
+        <div class="col-sm-6">              
+          <div class="input-group has-validation">
+            <span class="input-group-text">Surname</span>
+            <input type="text" class="form-control" id="surname" name="surname" placeholder="JORDAN" required="" v-model="personne.surname">
+            <div class="invalid-feedback">
+              The surname is required.
+            </div>
+          </div>
+          <br/>
         </div>
+
+        <div class="col-sm-6">              
+          <div class="input-group has-validation">
+            <span class="input-group-text">Name</span>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Michael" required="" v-model="personne.name">
+            <div class="invalid-feedback">
+              The name is required.
+            </div>
+          </div>
+          <br/>
+        </div>
+
+        <div class="col-sm-6">              
+          <div class="input-group has-validation">
+            <span class="input-group-text">Phone</span>
+            <input type="text" class="form-control" id="phone" name="phone" placeholder="000-0000-000" required="" v-model="personne.phone">
+            <div class="invalid-feedback">
+              The phone # is required.
+            </div>
+          </div>
+          <br/>
+        </div>
+
+        <div class="col-sm-6">              
+          <div class="input-group has-validation">
+            <span class="input-group-text">City</span>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Chicago" required="" v-model="personne.city">
+            <div class="invalid-feedback">
+              The city value is required.
+            </div>
+          </div>
+          <br/>
+        </div>    
+        
+
+        <!--
+          <div class="col-sm-9">
+            <label for="name" class="form-label">Surname</label>
+            <input type="text" class="form-control" id="surname" name="surname" v-model="personne.surname">
+          </div>
+
+          <div class="col-sm-9">
+            <label for="surname" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" name="name" v-model="personne.name">
+          </div>
+
+          <div class="col-sm-9">
+            <label for="phone" class="form-label">Phone</label>
+            <input type="text" class="form-control" id="phone" name="phone" v-model="personne.phone">
+          </div>
+          
+          <div class="col-sm-9">
+            <label for="city" class="form-label">City</label>
+            <input type="text" class="form-control" id="city" name="city" v-model="personne.city">
+          </div>
+        -->
 
         <div class="col-sm-9">       
           <p>{{ message }}</p>       
-          <button @click="creerPersonne" class="btn btn-success">Ajouter</button>
+          <!--
+            <button @click="creerPersonne" class="btn btn-success">Ajouter</button>
+          -->
+          <button @click="creerPersonne" class="w-20 btn btn-lg btn-primary">Ajouter</button>
         </div>
 
         
--- src/components/Personne.vue
+++ src/components/Personne.vue
@@ -10,27 +10,49 @@
       <!--<form method="post" action="/modifier-personne"> -->
         <input type="hidden" name="id" v-model="currentPersonne.id">
 
-        <div class="col-sm-6">
-          <label for="surname" class="form-label">Surname</label>
-          <input type="text" class="form-control" id="surname" name="surname" v-model="currentPersonne.surname">
+        <div class="col-sm-6">              
+          <div class="input-group has-validation">
+            <span class="input-group-text">Surname</span>
+            <input type="text" class="form-control" id="surname" name="surname" placeholder="currentPersonne.surname" required="" v-model="currentPersonne.surname">
+            <div class="invalid-feedback">
+              The surname is required.
+            </div>
+          </div>
+          <br/>
         </div>
 
-
-        <div class="col-sm-6">
-          <label for="name" class="form-label">Name</label>
-          <input type="text" class="form-control" id="name" name="name" v-model="currentPersonne.name">
+        <div class="col-sm-6">              
+          <div class="input-group has-validation">
+            <span class="input-group-text">Name</span>
+            <input type="text" class="form-control" id="name" name="name" placeholder="currentPersonne.name" required="" v-model="currentPersonne.name">
+            <div class="invalid-feedback">
+              The name is required.
+            </div>
+          </div>
+          <br/>
         </div>
 
-        <div class="col-sm-6">
-          <label for="phone" class="form-label">Phone</label>
-          <input type="text" class="form-control" id="phone" name="phone" v-model="currentPersonne.phone">
+        <div class="col-sm-6">              
+          <div class="input-group has-validation">
+            <span class="input-group-text">Phone</span>
+            <input type="text" class="form-control" id="phone" name="phone" placeholder="currentPersonne.phone" required="" v-model="currentPersonne.phone">
+            <div class="invalid-feedback">
+              The phone # is required.
+            </div>
+          </div>
+          <br/>
         </div>
-        
-        <div class="col-sm-6">
-          <label for="city" class="form-label">City</label>
-          <input type="text" class="form-control" id="city" name="city" v-model="currentPersonne.city">
+
+        <div class="col-sm-6">              
+          <div class="input-group has-validation">
+            <span class="input-group-text">City</span>
+            <input type="text" class="form-control" id="city" name="city" placeholder="currentPersonne.city" required="" v-model="currentPersonne.city">
+            <div class="invalid-feedback">
+              The city value is required.
+            </div>
+          </div>
+          <br/>
         </div>
-        <br/>
 
         <!-- A INCLURE DANS LE FORM -->
          &nbsp; 
